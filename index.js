ded63d619812129fb92db473663bd17be011d013
Oladimeji Odusote <taiwo.odusote@yahoo.com>
2023-01-31 22:32:33 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: "Ola",
+    streetAddress: "11507 St. Germain Wat" 
+}
+
+function updateEmployeeWithKeyAndValue(Object, key, value){
+    const newObject = { ...Object};
+    newObject[key] = value;
+    return newObject;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(Object, key, value){
+    Object[key] = value;
+    return Object;
+}
+
+function deleteFromEmployeeByKey(Object, key){
+    const newObject = {...Object};
+    delete newObject[key];
+    return newObject;
+}
+
+function destructivelyDeleteFromEmployeeByKey(Object,key){
+    delete Object[key];
+    return Object;
+}
